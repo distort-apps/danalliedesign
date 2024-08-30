c3febb6f10e0d8addf93b983fc36de5abbded4fe
Daniel <danoladesign@gmail.com>
2024-08-30 03:41:39 -0400

    DD007: Updating Carousel

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><MainHeader/>{children}<MainFooter/></body>
+      <body className={inter.className}><MainHeader/>{children}</body>
     </html>
   );
 }
