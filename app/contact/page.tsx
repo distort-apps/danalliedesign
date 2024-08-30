2376a12e121abfe82ecf324385bdbfa5a5469b94
Daniel <danoladesign@gmail.com>
2024-08-29 21:58:29 -0400

    DD003: Contact Page -implemented contact page -implemented contact.module.css

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,27 @@
-import classes from './page.module.css'
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function ContactPage() {
-    return <main className={classes.main}>
-        hello
-    </main>
+  return (
+    <div className={classes.contact}>
+      <header className={classes.header}>
+        <h1>Contact DanAllie Design</h1>
+        <p>
+          If you have any inquiries about my projects, or if youre looking for plans, we would love to connect:
+        </p>
+      </header>
+      <main className={classes.info}>
+        <p>Email:{" "}
+          <a href="mailto:danielgene.dev@gmail.com" className={classes.link}>
+            danalliedesign@gmail.com
+          </a>
+        </p>
+        <p>LinkedIn:{" "}
+          <Link href="https://www.linkedin.com/in/danicodes01/" className={classes.link}>
+            connect with us on Instagram
+          </Link>
+        </p>
+      </main>
+    </div>
+  );
 }
